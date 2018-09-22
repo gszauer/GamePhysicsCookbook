e8b1e24ac94a1f8b502b5f7366e472aec7c976d1
Gabor Szauer <git@szauer.net>
2018-09-22 15:11:00 -0700

    Switched to gh-pages

--- Code/IMGUIWindow.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_IMGUI_WINDOW_
-#define _H_IMGUI_WINDOW_
-
-// TODO: Implement DearImgui as a standard, sub-classable window
-// url: https://github.com/ocornut/imgui
-
-#include "GLWindow.h"
-
-#endif
--- Code/ObjLoader.h
+++
@@ -1,9 +0,0 @@
-#ifndef _H_OBJ_LOADER_
-#define _H_OBJ_LOADER_
-
-#include "Geometry3D.h"
-
-bool LoadMesh(const char* szFilePath, Mesh* mesh);
-void FreeMesh(Mesh* mesh);
-
-#endif 
